7e9eef9c216803bb24371d487f7267265f5ebb19
fevziatanoglu <fevziatan2@gmail.com>
2024-04-09 01:04:14 +0300

    Error Checking and Fixes

--- app/page.js
+++ app/page.js
@@ -1,28 +1,48 @@
 "use client";
-import { createPdf } from "@/lib/createPdf";
 import { createPptx } from "@/lib/createPptx";
 import { multiplyString, splitString } from "@/lib/editString";
 import { getImageSrc } from "@/lib/getImageSrc";
 import { useState } from "react";
 
 export default function Home() {
+  // variables
   const [isLoading, setLoading] = useState(false);
   const [slideShareUrl, setSlideShareUrl] = useState("");
-
+  const [error, setError] = useState("");
+  // click function
   const handleClick = async () => {
     try {
       setLoading(true);
+      // check is url slideshare link
+      if (!slideShareUrl.startsWith("https://www.slideshare.net/")) {
+        // if not set error
+        setError('Link have to starts with "https://www.slideshare.net". Please check again.');
+        setSlideShareUrl('');
+        return
+      }
+      // get images datas
       const { counter, firstImage } = await getImageSrc(slideShareUrl);
+      // if no data set error
+      if (counter === 0 || firstImage === undefined) {
+        setError('Presentation could not be found. Please check your link again.');
+        return;
+      }
+      // set images by using first image and counter
       const imageSrc = splitString(firstImage);
       const imageSrcs = multiplyString(counter, imageSrc);
-      // createPptx fonksiyonunu çağıralım
+      // create pptx file
       await createPptx(imageSrcs);
       setLoading(false);
-    } catch (err) {
-      console.log(err);
+      setError('');
+      // catch error
+    } catch (error) {
+      setError(error.message || error);
+      setLoading(false);
+    } finally {
+      setLoading(false);
     }
   };
-
+  // html elements
   return (
     <main className="h-screen flex flex-col pt-32 px-4 lg:p-0 lg:justify-center items-center lg:gap-8 gap-4 bg-slate-950">
       {/* texts */}
@@ -52,18 +72,17 @@ export default function Home() {
         <input
           className="w-full  text-[10px] lg:text-lg text-white placeholder:text-gray-500 lg:p-4 px-3 py-2 rounded-2xl lg:border-[4px] border-2 border-blue-500 bg-transparent"
           placeholder="Enter slideshare presentation link here"
-          onChange={(e) => {
-            setSlideShareUrl(e.target.value);
-            console.log(slideShareUrl);
-          }}
+          onChange={(e) => { setSlideShareUrl(e.target.value); }}
         />
         {/* download button */}
         <button
           className="bg-green-700 border-[4px] hover:border-white border-green-700  rounded-3xl lg:text-3xl text-xs font-bold lg:px-8 lg:py-4 px-2 py-1 disabled:opacity-50 hover:bg-green-600"
           onClick={(e) => handleClick()}
+          disabled={isLoading || slideShareUrl === ''}
         >
           {isLoading ? "LOADING..." : "DOWNLOAD"}
         </button>
+        <div className="text-red-400 text-[9px] font-normal lg:text-xl lg:font-semibold text-center h-1">{error}</div>
       </div>
     </main>
   );
